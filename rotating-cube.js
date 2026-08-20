c52654af35da4e381aed0e78973ebecf97f916fd
Nojjia <labaguettethe7th@gmail.com>
2026-08-20 22:58:58 +0400

    first commit

---
+++ rotating-cube.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("first").appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+
+
+camera.position.z = 5;
+
+scene.add(cube);
+
+function animate( time ) {
+	renderer.render(scene, camera);
+	cube.rotation.x = time / 1000;
+	cube.rotation.y = time / 1000;
+}
+
+renderer.setAnimationLoop(animate);
